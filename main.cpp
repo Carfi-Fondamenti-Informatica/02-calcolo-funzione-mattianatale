302ded13f8a735300c7e4c19d048d29319de851e
mattianatale <117930083+mattianatale@users.noreply.github.com>
2022-11-15 14:16:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
 using namespace std;
+float a,b,x,y,z ;
+    cin >> a >> b >> x >> y;
+    if (x<0 && y>0) {
+        cout << (z = (a*x)-(b*y)) << endl ;
 
-int main() {
+    }else if (x>=0 && y<=0) {
+            cout << (z=(a*(x*x))-(b*y)) << endl ;
+
+        }else {
+            cout<< (z=(a*x)+(b*(y*y)))<< endl ;
+
+        }
    return 0;
 }
